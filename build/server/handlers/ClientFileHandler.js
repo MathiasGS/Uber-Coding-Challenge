27a12f2de5a2ef208ccf520688dcb023980a82a1
Mathias Grund Sørensen <grund@Mathiass-MacBook-Pro.local>
2016-09-25 18:32:44 +0200

    Fix deployment.

--- build/server/handlers/ClientFileHandler.js
+++ build/server/handlers/ClientFileHandler.js
@@ -2,7 +2,7 @@
 var restify = require("restify");
 var getHandler = restify.serveStatic({
     default: "index.html",
-    directory: "client",
+    directory: "./client",
 });
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = getHandler;
