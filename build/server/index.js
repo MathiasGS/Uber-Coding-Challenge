133f12429c372103f2017c524dcdac07addcbf6e
Mathias Grund Sørensen <grund@Mathiass-MacBook-Pro.local>
2016-09-25 17:47:31 +0200

    .

--- build/server/index.js
+++ build/server/index.js
@@ -3,13 +3,8 @@ require('dotenv').config();
 var Application_1 = require("./Application");
 var SendGridAdapter_1 = require("./mailService/adapters/SendGridAdapter");
 var Worker_1 = require("./mailService/Worker");
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var workers = [];
-for (var i = 0; i < numCPUs; i++) {
-    var worker = new Worker_1.default([
-        new SendGridAdapter_1.default(),
-    ]);
-}
+var worker = new Worker_1.default([
+    new SendGridAdapter_1.default(),
+]);
 var app = new Application_1.default(worker);
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -4,16 +4,8 @@ import Server from "./Application";
 import SendGridAdapter from "./mailService/adapters/SendGridAdapter";
 import Worker from "./mailService/Worker";
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-let workers = [];
-
-for(let i = 0; i < numCPUs; i++){
 const worker = new Worker([
     new SendGridAdapter(),
 ]);
-}
-
 
 const app = new Server(worker);
